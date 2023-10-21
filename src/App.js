6c447f3f6c14065b42afdcafd354c96ddd43a2e4
Sterling Sanders <sterlingsanders723@gmail.com>
2023-10-20 18:54:31 -0700

    made counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [count, setCount] = useState(0)
+  const increaseClick = () => {
+    setCount(count + 1);
+  }
+  const decreaseClick = () => {
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <button onClick={increaseClick}>INCREASE</button>
+      <h2>{count}</h2>
+      <button onClick={decreaseClick}>DECREASE</button>
     </div>
   );
 }
